0daca9155f5f2bb996a1e9b50a29a26821fecc60
Dmitry Sysoev <dsysoev@protonmail.com>
2024-06-24 21:09:17 +0300

    update probability for random effect based on real data

--- src/get_score.py
+++ src/get_score.py
@@ -1,12 +1,13 @@
 import argparse
 import json
 from functools import partial
+from typing import Dict, NoReturn
 
 import numpy as np
 import pandas as pd
 
 
-def f_flip(x, name):
+def f_flip(x: pd.Series, name: str) -> pd.Series:
     first = x["first"]
     second = x["second"]
     score = x["score"]
@@ -15,44 +16,55 @@ def f_flip(x, name):
         first, second = second, first
         score *= -1.0
 
-    return pd.Series([first, score, second],
-                     index=["first", "score", "second"])
+    return pd.Series([first, score, second], index=["first", "score", "second"])
 
 
-def get_experiment_stat(data, rounds=10000):
-    def calc_p_value(reference_value, choice_list, size, rounds):
+def get_experiment_stat(
+    data: pd.DataFrame, probs_dict: Dict[float, float], rounds: int = 10000
+) -> Dict[str, float]:
+    def calc_p_value(reference_value, choice_list, size, rounds, probs):
         diff_list = []
         for i in range(rounds):
-            s_choice = np.random.choice(choice_list, replace=True, size=size)
+            s_choice = np.random.choice(
+                choice_list, replace=True, size=size, p=probs)
             if reference_value != 0.0:
                 diff_list.append(reference_value - s_choice.mean())
             else:
                 diff_list.append(s_choice.mean())
-        percentiles = np.round(np.percentile(a=diff_list, q=[5, 95]), 1)
+
+        percentiles = np.round(np.percentile(a=diff_list, q=[2.5, 97.5]), 1)
         a = np.array(diff_list)
         p = a[a > 0].shape[0] / a.shape[0]
         p = min(p, 1 - p)
         p = round(p * 100.0, 1)
         return round(np.mean(a), 1), list(percentiles), p
 
+    # set simple stats and prior probability
     win = int(data.loc["win"])
     draw = int(data.loc["draw"])
     lost = int(data.loc["lost"])
+    probs = [probs_dict[-1.0], probs_dict[0.0], probs_dict[1.0]]
 
+    # set a choice list for estimate mean effect
     choice_list = draw * [50.0] + lost * [0.0] + win * [100.0]
     m_original = sum(choice_list) / len(choice_list)
-
     size = len(choice_list)
+
+    # calculate mean effect
     effect, perc, p = calc_p_value(
-        reference_value=0, choice_list=choice_list, size=size, rounds=rounds
+        reference_value=0, choice_list=choice_list, size=size, rounds=rounds, probs=None
     )
 
+    # calculate clean effect
+    # clean effect = mean effect - random effect
     rand_effect, rand_perc, rand_p = calc_p_value(
         reference_value=m_original,
         choice_list=[0.0, 50.0, 100.0],
         size=size,
         rounds=rounds,
+        probs=probs,
     )
+
     return {
         "win": win,
         "draw": draw,
@@ -66,7 +78,7 @@ def get_experiment_stat(data, rounds=10000):
     }
 
 
-def f_compare(x):
+def f_compare(x: pd.Series) -> str:
     first = x["first"].split("-")[0]
     second = x["second"].split("-")[0]
 
@@ -76,7 +88,7 @@ def f_compare(x):
         return "vs higher"
 
 
-def get_result(df):
+def get_result(df: pd.DataFrame) -> pd.DataFrame:
     result = None
     for name in sorted(df["first"].unique()):
         f1 = partial(f_flip, name=name)
@@ -103,7 +115,8 @@ def get_result(df):
     return result
 
 
-def get_stats(df):
+def get_stats(df: pd.DataFrame, df_raw: pd.DataFrame) -> pd.DataFrame:
+    probs_dict = df_raw["score"].value_counts(normalize=True).to_dict()
     stats = []
     for bench in df["first"].unique():
         for compare in ["vs lower", "vs higher"]:
@@ -113,7 +126,7 @@ def get_stats(df):
                 .set_index("result")
             )
             if int(s.isna().sum() == 0):
-                results = get_experiment_stat(s, rounds=10000)
+                results = get_experiment_stat(s, probs_dict, rounds=10000)
                 results["name"] = bench
                 results["with"] = compare
                 stats.append(results)
@@ -122,7 +135,7 @@ def get_stats(df):
     return stats
 
 
-def load_stats(filepath):
+def load_stats(filepath: str) -> pd.DataFrame:
     with open(filepath, "r") as f:
         raw = json.load(f)
 
@@ -134,18 +147,19 @@ def load_stats(filepath):
     return df
 
 
-def main():
+def main() -> NoReturn:
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-s", "--stats", default="output/stats.json", type=str)
-    parser.add_argument(
-        "-o", "--output", default="output/score.md", type=str)
+    parser.add_argument("-s", "--stats", default="output/stats.json", type=str)
+    parser.add_argument("-o", "--output", default="output/score.md", type=str)
     args, _ = parser.parse_known_args()
 
+    # read file with all experiments
     df_raw = load_stats(args.stats)
+    # prepare group results dataframe
     df_group = get_result(df_raw)
-    stats = get_stats(df_group)
-
+    # calculate final stats
+    stats = get_stats(df_group, df_raw)
+    # save stats to markdown file
     (
         stats[
             [
